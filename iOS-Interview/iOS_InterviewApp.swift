d8a3ab94ac32479f314c91173d69d10cf8079a93
Phan Viet Trung <tghn@gft.com>
2025-09-05 09:49:47 +0700

    Initial Commit

---
+++ iOS-Interview/iOS_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_InterviewApp.swift
+//  iOS-Interview
+//
+//  Created by Phan, Trung on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
